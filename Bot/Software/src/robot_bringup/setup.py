c11b4e37a465facdfe35539f2e5fb09c193b6155
Nico <nicolasrdrgz@protonmail.com>
2025-04-15 21:00:56 -0700

    Set package to empty

--- Bot/Software/src/robot_bringup/setup.py
+++ Bot/Software/src/robot_bringup/setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = "robot_bringup"
 
 setup(
     name=package_name,
     version="0.0.1",
+    packages=[],  # Explicitly set to empty list
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
