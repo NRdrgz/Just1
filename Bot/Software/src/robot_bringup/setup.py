623ba0f831af26d217affbe6221327f5a9f08b4b
Nico <nicolasrdrgz@protonmail.com>
2025-04-15 20:59:22 -0700

    Change for launch only package

--- Bot/Software/src/robot_bringup/setup.py
+++ Bot/Software/src/robot_bringup/setup.py
@@ -5,7 +5,6 @@ package_name = "robot_bringup"
 setup(
     name=package_name,
     version="0.0.1",
-    packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
@@ -18,7 +17,4 @@ setup(
     description="Launch files and configurations for bringing up the robot",
     license="Apache License 2.0",
     tests_require=["pytest"],
-    entry_points={
-        "console_scripts": [],
-    },
 )
