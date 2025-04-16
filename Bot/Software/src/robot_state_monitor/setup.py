330f96bd76c5e9ed6d777f6e94ac386e11aac34a
Nico <nicolasrdrgz@protonmail.com>
2025-04-15 20:45:53 -0700

    Update setup

--- Bot/Software/src/robot_state_monitor/setup.py
+++ Bot/Software/src/robot_state_monitor/setup.py
@@ -6,7 +6,7 @@ setup(
     name=package_name,
     version="0.0.0",
     packages=[package_name],
-    package_dir={"": ""},  # This tells setuptools to look in the root directory
+    package_dir={"": "src"},  # This tells setuptools to look in the src directory
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
