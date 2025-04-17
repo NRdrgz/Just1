a4657a247037d2635da61503dd90d9b3a458ede2
Nico <nicolasrdrgz@protonmail.com>
2025-04-17 16:46:33 -0700

    Add init for msg

--- Bot/Software/src/manual_motor_controller/setup.py
+++ Bot/Software/src/manual_motor_controller/setup.py
@@ -9,7 +9,6 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name + "/msg", ["msg/WheelSpeeds.msg"]),
     ],
     install_requires=["setuptools", "rosidl_runtime_py"],
     zip_safe=True,
