132840cd2a2d1231248e9445644acf8e281aeeb9
Nico <nicolasrdrgz@protonmail.com>
2025-04-17 15:58:35 -0700

    Rename wheel speeds

--- Bot/Software/src/manual_motor_controller/setup.py
+++ Bot/Software/src/manual_motor_controller/setup.py
@@ -9,7 +9,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name + "/msg", ["msg/WheelSpeed.msg"]),
+        ("share/" + package_name + "/msg", ["msg/WheelSpeeds.msg"]),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
