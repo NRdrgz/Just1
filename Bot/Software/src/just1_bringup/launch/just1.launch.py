968d282ccbcddbf6a994efb6520a0ce5c5450ab3
Nico <nicolasrdrgz@protonmail.com>
2025-06-07 15:16:08 -0700

    Try zeta on IMU filtering

--- Bot/Software/src/just1_bringup/launch/just1.launch.py
+++ Bot/Software/src/just1_bringup/launch/just1.launch.py
@@ -189,6 +189,8 @@ def generate_launch_description():
         parameters=[
             {"use_mag": False},
             {"publish_tf": False},  # We let ICP deal with the odom->base_link tf
+            {"gain": 0.1},
+            {"zeta": 0.1},
         ],
     )
     ld.add_action(filter_imu_node)
