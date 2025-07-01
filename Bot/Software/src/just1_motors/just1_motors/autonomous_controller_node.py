009226ddf9475dbdc4f9d9a10813f34822676b5e
Nico <nicolasrdrgz@protonmail.com>
2025-06-30 20:16:34 -0700

    Add a minimum percentage for autonomous wheel speed

--- Bot/Software/src/just1_motors/just1_motors/autonomous_controller_node.py
+++ Bot/Software/src/just1_motors/just1_motors/autonomous_controller_node.py
@@ -123,6 +123,7 @@ class AutonomousMotorController(Node):
         # Convert RPM to percentage (-100 to 100)
         # Max forward speed is 0.5 m/s so max RPM is 123
         max_rpm = 123
+        min_effective_percent = 50
 
         for wheel_name, rpm in wheel_speeds.items():
             # Clamp RPM to max_rpm
@@ -130,6 +131,12 @@ class AutonomousMotorController(Node):
 
             # Convert to percentage
             percentage = int((rpm / max_rpm) * 100)
+
+            # Apply minimum effective percentage
+            if percentage > 0 and percentage < min_effective_percent:
+                percentage = min_effective_percent
+            elif percentage < 0 and percentage > -min_effective_percent:
+                percentage = -min_effective_percent
 
             # Control the wheel
             control_wheel(wheel_name, percentage)
