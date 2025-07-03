9492657434477a000cbcb5fc80ded640f2bb88df
Nico <nicolasrdrgz@protonmail.com>
2025-07-02 20:37:49 -0700

    Add params for icp odom and max translation

--- Bot/Software/src/just1_bringup/launch/just1.launch.py
+++ Bot/Software/src/just1_bringup/launch/just1.launch.py
@@ -376,6 +376,8 @@ def generate_launch_description():
             {"scan_topic": "/scan"},  # 2D Lidar topic
             {"publish_tf": True},  # Publishes TF from odom -> base_link
             {"queue_size": 10},
+            {"Icp/MaxTranslation": 0.5},  # Maximum translation acceptance (m)
+            {"Icp/MaxRotation": 1.57},  # Maximum rotation acceptance (rad)
         ],
         remappings=[
             ("/imu", "/imu/synced"),
